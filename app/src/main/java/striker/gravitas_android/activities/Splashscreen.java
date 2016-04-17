eaa447bd1cb49b90996c2f3161eebf73c65183a9
striker <prateektank120@gmail.com>
2016-04-17 17:54:05 +0530

    DB

--- app/src/main/java/striker/gravitas_android/activities/Splashscreen.java
+++ app/src/main/java/striker/gravitas_android/activities/Splashscreen.java
@@ -10,6 +10,6 @@ public class Splashscreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splashscreen);
     }
 }
