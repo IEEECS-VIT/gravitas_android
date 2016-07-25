141f4a3a8527ba65c2ab7aaea4e280ca48459751
waris0023 <waris0023@gmail.com>
2016-07-26 00:23:25 +0530

    Navigation drawer added.

--- app/src/main/java/striker/gravitas_android/activities/Home.java
+++ app/src/main/java/striker/gravitas_android/activities/Home.java
@@ -1,15 +1,161 @@
 package striker.gravitas_android.activities;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import striker.gravitas_android.R;
+import striker.gravitas_android.navigationDrawer.adapter.CustomExpandableListAdapter;
+import striker.gravitas_android.navigationDrawer.datasource.ExpandableListDataSource;
 
 public class Home extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String mActivityTitle;
+
+    private ExpandableListView mExpandableListView;
+    private ExpandableListAdapter mExpandableListAdapter;
+    private List<String> mExpandableListTitle;
+    private Map<String, List<String>> mExpandableListData;
+    private TextView mSelectedItemView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setNavigationDrawer();
+    }
+
+    private void setNavigationDrawer() {
         setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
+
+        mExpandableListView = (ExpandableListView) findViewById(R.id.navList);
+        mSelectedItemView = (TextView) findViewById(R.id.selected_item);
+
+        LayoutInflater inflater = getLayoutInflater();
+        //View listHeaderView = inflater.inflate(R.layout.nav_header, null, false);
+        //mExpandableListView.addHeaderView(listHeaderView);
+
+        mExpandableListData = ExpandableListDataSource.getData(this);
+        mExpandableListTitle = new ArrayList(mExpandableListData.keySet());
+
+        addDrawerItems();
+        setupDrawer();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+    }
+
+    private void addDrawerItems() {
+        mExpandableListAdapter = new CustomExpandableListAdapter(this, mExpandableListTitle, mExpandableListData);
+        mExpandableListView.setAdapter(mExpandableListAdapter);
+        //  mExpandableListView.setGroupIndicator(null);
+        mExpandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                getSupportActionBar().setTitle(mExpandableListTitle.get(groupPosition).toString());
+                mSelectedItemView.setText(mExpandableListTitle.get(groupPosition).toString());
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+            }
+        });
+
+        mExpandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+            }
+        });
+
+        mExpandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                String selectedItem = ((List) (mExpandableListData.get(mExpandableListTitle.get(groupPosition))))
+                        .get(childPosition).toString();
+                getSupportActionBar().setTitle(selectedItem);
+                mSelectedItemView.setText(mExpandableListTitle.get(groupPosition).toString() + " -> " + selectedItem);
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                return false;
+            }
+        });
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        // Activate the navigation drawer toggle
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/striker/gravitas_android/navigationDrawer/datasource/ExpandableListDataSource.java
@@ -0,0 +1,41 @@
+package striker.gravitas_android.navigationDrawer.datasource;
+
+import android.content.Context;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import striker.gravitas_android.R;
+
+/**
+ * Created by HP on 7/25/2016.
+ */
+public class ExpandableListDataSource {
+    /**
+     * Returns data of navigation drawer.
+     *
+     * @param context
+     * @return
+     */
+    public static Map<String, List<String>> getData(Context context) {
+        Map<String, List<String>> expandableListData = new TreeMap<>();
+
+        List<String> navHeading = Arrays.asList(context.getResources().getStringArray(R.array.nav_list));
+
+        List<String> home = Arrays.asList(context.getResources().getStringArray(R.array.Home));
+        List<String> wishlist= Arrays.asList(context.getResources().getStringArray(R.array.Wishlist));
+        List<String> categories = Arrays.asList(context.getResources().getStringArray(R.array.Categories));
+        List<String> about = Arrays.asList(context.getResources().getStringArray(R.array.About));
+        List<String> contact = Arrays.asList(context.getResources().getStringArray(R.array.contact));
+
+        expandableListData.put(navHeading.get(0), home);
+        expandableListData.put(navHeading.get(1), wishlist);
+        expandableListData.put(navHeading.get(2), categories);
+        expandableListData.put(navHeading.get(3), about);
+        expandableListData.put(navHeading.get(4), contact);
+
+        return expandableListData;
+    }
+}
